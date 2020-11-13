3cfa9a294ec7ecd231da0a1ab7455492387eb2b3
Abhinav kumar <42245122+singhdotabhinav@users.noreply.github.com>
2020-11-14 01:42:46 +0530

    Update MinMax.cpp

--- MinMax.cpp
+++ MinMax.cpp
@@ -1,8 +1,7 @@
-Simple iterative approach
+//Simple iterative approach
 #include <bits/stdc++.h>
 
 using namespace std;
-
 void MinMax(int arr[], int n){
 
     // Initialize both max and min to first element of array
@@ -26,3 +25,60 @@ int main(){
     MinMax(arr,n);
     return 0;
 }
+
+
+//Divide and conquer approach
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static int arr[100];
+
+void MaxMin(int a[],int low,int high, int *max, int *min){
+
+    int mid,max1,max2,min1,min2;
+    if(high-low == 1)
+    {
+        if(a[low] > a[high]){
+            *max = a[low];
+            *min = a[high];
+        }
+        else{
+            *max = a[high];
+            *min = a[low];
+        }
+    }
+    else if(low == high){
+        *min = *max = a[low];
+    }
+    else if(low<high)
+    {
+        mid=(low+high)/2;
+        MaxMin(a,low,mid,&max1,&min1);
+        MaxMin(a,mid+1,high,&max2,&min2);
+            if(max1 > max2)
+                *max = max1;
+            else
+                *max = max2;
+            if(min1 < min2)
+                *min = min1;
+            else
+                *min = min2;
+    }
+}
+
+int main(){
+
+
+    int arr[] = {1, 2, 3, 4, 5, 6,7};
+    int n = sizeof(arr)/ sizeof(arr[0]);
+    int Min=arr[0];
+    int Max=arr[0];
+
+    //MinMax(0,n-1,Min, Max);
+    MaxMin(arr, 0, n-1, &Max, &Min);
+    cout<<Max<<"\t"<<Min;
+    return 0;
+}
+//Make use of global variable if afraid of using pointer
+
